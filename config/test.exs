d63a835e7cf1b14b9af642dc732d346ebfb0c717
Matt Silbernagel <silbermm@gmail.com>
2017-12-16 13:27:45 -0500

    remove extra | from config file

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :twixir, Twixir.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("POSTGRES_USER") || "postgres",                                                             │
+  username: System.get_env("POSTGRES_USER") || "postgres",
   password: System.get_env("POSTGRES_PASSWORD") || "postgres",
   database: "twixir_test",
   hostname: "localhost",
