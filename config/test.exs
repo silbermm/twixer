34f4f97b277a99e03467a08ef6f4aed5fa03ba31
Matt Silbernagel <silbermm@gmail.com>
2017-12-16 13:22:33 -0500

    test configuration for CI

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :twixir, Twixir.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("POSTGRES_USER") || "postgres",                                                             │
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
   database: "twixir_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
