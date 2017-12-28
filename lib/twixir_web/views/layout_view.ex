5fd17008dad6fbbba572d68df755bb2de7b1cb50
Matt Silbernagel <silbermm@gmail.com>
2017-12-28 12:42:21 -0500

    add the explore link and show active status

--- lib/twixir_web/views/layout_view.ex
+++ lib/twixir_web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule TwixirWeb.LayoutView do
   use TwixirWeb, :view
+
+  def active_class(%{request_path: current_path} = _conn, path) when current_path == path do
+    "active"
+  end
+  def active_class(_conn, _path), do: ""
 end
