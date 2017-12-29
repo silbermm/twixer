20ca0515f4ef24ac81eea3be591a705bc7abf15c
Matt Silbernagel <silbermm@gmail.com>
2017-12-29 16:33:19 -0500

    start the search functionality

--- lib/twixir_web/router.ex
+++ lib/twixir_web/router.ex
@@ -32,6 +32,8 @@ defmodule TwixirWeb.Router do
 
     get "/explore", ExploreController, :index
 
+    post "/search/user/:search_term", SearchController, :find_user
+
     get "/:user_id",        PageController, :show_user
     get "/follow/:user_id", PageController, :follow
     get "/",                PageController, :index
